462531ce9bb98a43cdc60923157f73fd4846ff25
kulkarnishubh1997 <kulkarnishubh1997@gmail.com>
2021-12-28 13:57:55 +0530

    first commit

---
+++ pro4/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def base(request):
+    return render(request,"base.html")
+
+def child(request):
+    return render(request,"child.html")
+
+def child1(request):
+    return render(request,"child1.html")
+
+
+
+
+
+
